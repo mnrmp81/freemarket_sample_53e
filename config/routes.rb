e5ecd7272c2572c4b5698227a3793cfd09e1c4c0
yfmk <cantocantocantocanto@gmail.com>
2019-06-28 15:54:06 +0900

    routes,controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,16 @@ Rails.application.routes.draw do
   resources :posts
   resources :mypages, only: :index
   root 'posts#index'
+  resources :profiles do
+    collection do
+      get 'new_1'
+      get 'new_2'
+      get 'new_3'
+      get 'new_4'
+      get 'new_5'
+      get 'new_6'
+      get 'new_7'
+    end
+  end
+    
 end
