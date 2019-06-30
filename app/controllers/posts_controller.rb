c4cd4f27f212350bf8cea481736c667a8a362fd6
mnrmp81 <81mnrmp@gmail.com>
2019-06-30 16:49:59 +0900

    edit posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,32 @@ class PostsController < ApplicationController
     @prefectures = Prefecture.all
   end
 
+  def create
+    Post.create(post_params)
+  end 
+
   def show
   end
 
   def buy
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:product_name,
+                                 :product_description,
+                                 :first_category_id,
+                                 :second_category_id,
+                                 :third_category_id,
+                                 :brand_id,
+                                 :product_size,
+                                 :product_condition,
+                                 :delivery_fee,
+                                 :delivery_former_area,
+                                 :delivery_date,
+                                 :product_price,
+                                 :user_id,
+                                 )
   end
 end
