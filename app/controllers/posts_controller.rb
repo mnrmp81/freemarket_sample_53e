c1170d4708a9d6351b0e4cb8fb58c4016855cb09
mnrmp81 <81mnrmp@gmail.com>
2019-07-01 17:22:28 +0900

    add error_handring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
   end 
 
   def show
