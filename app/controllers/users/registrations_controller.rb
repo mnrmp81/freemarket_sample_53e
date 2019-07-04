1ecb865c0c359c466f8a287075e0e54645a4f6a2
riri0427 <riri190427@gmail.com>
2019-07-04 16:38:55 +0900

    Remove unneccesary written of registrations controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-    @user = User.new
-    @user.build_profile
-    super
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource
   def create
@@ -46,8 +44,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # private
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [profile_attributes: [:profile, :family_name, :first_name, :family_name_kana, :first_name_kana, :phone_number]])
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
   end
 
   # If you have extra params to permit, append them to the sanitizer.
