e84ebe808060c2aa5c9b4c9f1ba04e40aa4c15c3
riri0427 <riri190427@gmail.com>
2019-07-04 16:51:14 +0900

    Add validations to address model when create record

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :postal_code, presence: true
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :block, presence: true
 end
