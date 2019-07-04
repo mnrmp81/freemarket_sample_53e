0a43d3c06b63a391126499cf26105be7d6aa5a7d
riri0427 <riri190427@gmail.com>
2019-07-04 16:50:48 +0900

    Add validations to profile model when create record

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
 end
