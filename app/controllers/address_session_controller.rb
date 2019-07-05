39975e372f9aaada74589fec11b46b3a82d5e8b1
riri0427 <riri190427@gmail.com>
2019-07-05 10:49:21 +0900

    Copy the code of session_address action in user_session controller to address_session controller
    1. user_session controller から以下の部分をコピー
    ・address_session アクションの記述全て
    ・address_session のストロングパラメータの記述全て
    
    2. address_session アクションの名前をcreateに変更

--- app/controllers/address_session_controller.rb
+++ app/controllers/address_session_controller.rb
@@ -1,2 +1,27 @@
 class AddressSessionController < ApplicationController
+  def create
+    # 送られてきた住所情報の中に空の値がないかを判定(building以外)
+    address_session_params.each do |key, value|
+      if key != 'building'
+        if value == ""
+          redirect_to new_4_profiles_path
+          return
+        end
+      end
+    end
+
+    session[:postal_code] = address_session_params[:postal_code]
+    session[:prefecture] = address_session_params[:prefecture]
+    session[:city] = address_session_params[:city]
+    session[:block] = address_session_params[:block]
+    session[:building] = address_session_params[:building]
+
+    redirect_to controller: :profiles, action: :new_5
+  end
+
+  private
+
+  def address_session_params
+    params.permit(:postal_code, :prefecture, :city, :block, :building)
+  end
 end
