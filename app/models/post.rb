65ca577a603252c0b31b895444b8cb59d5c0fe09
shirollo7 <shinji.noshiro@gmail.com>
2019-07-05 16:41:23 +0900

    test

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
   
+  
   with_options presence: true do
     validates :images
     validates :product_name
