5efd5440f59f97f1e67e40512047b65a9555af8c
ryowatabe <42095725+ryowatabe@users.noreply.github.com>
2019-07-05 16:50:03 +0900

    add-callback-processing
    コールバックした後のユーザ登録、ユーザログイン機能を実装した
    (まだ未完成だが、Viewが足りていないためマージさせて下さい)

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    callback_for(:facebook)
+  end
+  
+  def google_oauth2
+    callback_for(:google)
+  end
+
+  def callback_for(provider)
+    @user = User.find_oauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in @user, event: :authentication
+      redirect_to after_sign_in_path_for(resource)
+    else
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to after_sign_in_path_for(resource)
+    end
+
+    def failure
+      redirect_to root_path and return
+    end
+
+    def after_sign_in_path_for(resource)
+      root_path
+    end
+  end
+
+  
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  # You should also create an action method in this controller like this:
+  # def twitter
+  # end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable
 
   has_many :orders
   has_many :posts
@@ -10,4 +11,16 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_one :address, dependent: :destroy
   has_many :credit_cards, dependent: :destroy
+
+  def self.find_oauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+    User.create_with(
+      uid: uid,
+      provider: provider,
+      nickname: auth.info.name,
+      email: auth.info.email,
+      password: Devise.friendly_token[0, 20]
+    ).find_or_create_by(uid: uid, provider: provider)
+  end
 end
