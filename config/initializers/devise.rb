d5cff10c19bea8c80b137026566e3de3c94339ae
ryowatabe <42095725+ryowatabe@users.noreply.github.com>
2019-07-05 16:45:43 +0900

    add-omniauth-ENV
    Facebook,Googleの環境変数を
    初期設定に追加した

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -283,6 +283,8 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET_KEY']
+  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'], { :skip_jwt => true }
 
   # ==> Turbolinks configuration
   # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
