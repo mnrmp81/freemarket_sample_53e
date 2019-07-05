c9ef98e873138decb263cfa5925683a81e49889d
Taisuke Miyazaki <53.miyazaki.taisuke@gmail.com>
2019-07-05 20:38:34 +0900

    storage

--- config/deploy.rb
+++ config/deploy.rb
@@ -72,8 +72,6 @@ end
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
-append :linked_files, 'config/master.key'
-
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
