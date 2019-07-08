e8743a469b7ec965914dd68b5cc5c54323d17b7c
riri0427 <riri190427@gmail.com>
2019-07-08 14:24:47 +0900

    Define variable in new action of profiles controller and address controller
    バリデーションエラーで弾かれた際、リダイレクト先で入力値を表示するための変数を定義

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,20 @@
 class ProfilesController < ApplicationController
-  
   def new
-  end
+    @minimum_password_length = 6
 
+    if session[:user]
+      @user = User.new(email: session[:user]["email"], nickname: session[:user]["nickname"])
+    else
+      @user = User.new
+    end
+    if session[:profile]
+      @profile = Profile.new(session[:profile])
+    else
+      @profile = Profile.new
+    end
+
+    # セッションを削除
+    session[:user] = nil
+    session[:profile] = nil
+  end
 end
