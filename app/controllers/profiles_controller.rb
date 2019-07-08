8827ca3cf7e27af84c30b64e64fed5901e32bb63
shirollo7 <shinji.noshiro@gmail.com>
2019-07-08 12:51:19 +0900

    change contrller
    コンフリクト解消の際に過去の修正も足してしまったっため。

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,19 +3,4 @@ class ProfilesController < ApplicationController
   def new
   end
 
-  def new_4
-  end
-
-  def new_5
-  end
- 
-  def new_6
-  end
-
-  def new_7
-  end
-
-  def edit
-  end
-
 end
