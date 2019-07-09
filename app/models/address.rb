acace0aef902b2e5fc13d58e41aae869624f7592
riri0427 <riri190427@gmail.com>
2019-07-09 17:02:46 +0900

    Add validation of address
    ・addressモデルにバリデーションを追加
    ・エラーメッセージを編集

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :user
 
-  validates :postal_code, presence: true
-  validates :prefecture, presence: true
-  validates :city, presence: true
-  validates :block, presence: true
+  validates :postal_code, presence: { message: '入力してください' }
+  validates :postal_code, length: { is: 7, message: 'フォーマットが正しくありません' }, if: ->(u) { u.postal_code.present? },
+    numericality: { only_integer: true, message: '' }
+  validates :prefecture, presence: { message: '選択してください' }
+  validates :city, presence: { message: '入力してください' }
+  validates :block, presence: { message: '入力してください' }
 end
