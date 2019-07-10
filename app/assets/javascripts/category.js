564d8707f229cbd2cdedab8d8e963af1ce16c268
mnrmp81 <81mnrmp@gmail.com>
2019-07-10 21:51:34 +0900

    modify option values
    name属性が誤っていたので修正
    optionのvalueをenumに対応

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -26,25 +26,23 @@ $(function () {
   });
 });
 
-// サイズ(enum)、ブランド(入力)フォーム
-// optionは仮置き
 $(function () {
   $(document).one("change", "#post_third_category_id", function () {
     var sizeHTML = `<label for="post_product_size">サイズ</label>
                     <span class="require__form">必須</span>
                     <div class="select__box">
-                      <select class="select__box__format" name="post[third_product_size]" id="post_product_size">
+                      <select class="select__box__format" name="post[product_size]" id="post_product_size">
                         <option>---</option>
-                        <option>XXS以下</option>
-                        <option>XS(SS)</option>
-                        <option>S</option>
-                        <option>M</option>
-                        <option>L</option>
-                        <option>XL(LL)</option>
-                        <option>2XL(3L)</option>
-                        <option>3XL(4L)</option>
-                        <option>4XL(5L)以上</option>
-                        <option>FREE SIZE</option>
+                        <option value="xxs_or_less">XXS以下</option>
+                        <option value="xs">XS(SS)</option>
+                        <option value="small">S</option>
+                        <option value="middle">M</option>
+                        <option value="large">L</option>
+                        <option value="xl">XL(LL)</option>
+                        <option value="xxl">2XL(3L)</option>
+                        <option value="xxxl">3XL(4L)</option>
+                        <option value="xxxxl_or_more">4XL(5L)以上</option>
+                        <option value="free">FREE SIZE</option>
                       </select>
                       <div class="drop-down-icon">
                         <i class="fas fa-chevron-down"></i>
