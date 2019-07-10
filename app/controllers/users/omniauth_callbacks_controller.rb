fddc6a8f49bad88e93812a0d935e437fdd0d431b
ryowatabe <42095725+ryowatabe@users.noreply.github.com>
2019-07-11 00:00:23 +0900

    Update omniauth_callbacks_controller.rb
    コールバック後の処理を変更
    もし、登録されているユーザであればログイン
    されていなければ、sessionを使用して新規登録画面に情報を持って遷移させる

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,12 +11,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_for(provider)
     @user = User.find_oauth(request.env["omniauth.auth"])
-    if @user.persisted?
+    if @user.present?
       sign_in @user, event: :authentication
       redirect_to after_sign_in_path_for(resource)
     else
-      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
-      redirect_to after_sign_in_path_for(resource)
+      auth = request.env["omniauth.auth"]
+      user = {
+        uid: auth.uid,
+        provider: provider,
+        nickname: auth.info.name,
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20]
+      }
+      session[:devise_omniauth_data] = user
+      redirect_to new_profile_path
     end
 
     def failure
@@ -27,6 +35,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       root_path
     end
   end
+
+  
 
 
   # You should configure your model like this:
