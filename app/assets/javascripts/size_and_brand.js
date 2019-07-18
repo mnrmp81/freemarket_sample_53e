05d6b309e0352e52e87055c54db97c5b6201e2dc
mnrmp81 <81mnrmp@gmail.com>
2019-07-18 17:16:45 +0900

    update size_and_brand.js
    カテゴリーの内容によってサイズ・ブランドフォームの出現を分岐

--- app/assets/javascripts/size_and_brand.js
+++ app/assets/javascripts/size_and_brand.js
@@ -1,13 +1,6 @@
-// （カテゴリー実装後のタスク）
-// 第３カテゴリーのバリューによってappendを条件分岐
-
 $(function () {
-  $(document).on("change", "#post_third_category_id", function () {
-    // var input = $(this).val();
-    // if (input == "カテゴリー実装後に記述") {
-      
-    // } 
-    var sizeHTML = `<label for="post_product_size">サイズ</label>
+  function sizeHTML() {
+    var html = `<label for="post_product_size">サイズ</label>
                     <span class="require__form">必須</span>
                     <div class="select__box">
                       <select class="select__box__format" name="post[product_size]" id="post_product_size">
@@ -27,24 +20,39 @@ $(function () {
                         <i class="fas fa-chevron-down"></i>
                       </div>
                     </div>`
-
-    var brandHTML = `<label for="product_brand_name">ブランド</label>
+    return html
+  }
+    
+  function brandHTML() {
+    var html = `<label for="product_brand_name">ブランド</label>
                      <span class="any__form">任意</span>
                      <div class="select__box">
                        <input type="text" name="post[brand_name]" class="select__box__format" placeholder="例)シャネル" id="brand_new"></input>
                      </div>`
+    return html
+  }
 
-    if ($('#post_product_size').length == 0) {
-      $('#size-content').append(sizeHTML);
-      $('#brand-content').append(brandHTML);
+  $(document).on("change", "#post_third_category_id", function () {
+    var input = $('#post_first_category_id').val();
+    if (input > 2 ) {
+      $('#size-content').empty();
+    } else {
+      if ($('#post_product_size').length == 0) {
+        var html = sizeHTML();
+        $('#size-content').append(html);
+      }
     }
   });
 
-  // 第３カテゴリーが〇〇以外に変わったらフォームを消す
-  // $(document).on("change", "#post_third_category_id", function () {
-  //   var input = $(this).val();
-  //   if (input == "カテゴリー実装後に記述") {
-  //     $('#size-content').remove();
-  //   } 
-  // });
+  $(document).on("change", "#post_third_category_id", function () {
+    var input = $('#post_first_category_id').val();
+    if (input == 5 || input == 10 || input == 11 ) {
+      $('#brand-content').empty();
+    } else {
+      if ($('#brand_new').length == 0) {
+        var html = brandHTML();
+        $('#brand-content').append(html);
+      }
+    }
+  });
 });
