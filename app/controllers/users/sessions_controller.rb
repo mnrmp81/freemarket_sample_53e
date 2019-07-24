8065e25341d0a1a56551cdaabf57038c23b4ff1b
riri0427 <riri190427@gmail.com>
2019-07-24 12:46:11 +0900

    Create error messages when params is blank
    コントローラ内でメールアドレス、パスワードが入力されなかった場合のエラーメッセージを作成

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -10,14 +10,20 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
+    
+    # メールアドレス、パスワードが入力されなかった場合のエラーメッセージ
+    flash[:blank] = blank_error_message(login_params)
+    
     if Rails.env == "production"
       if verify_recaptcha.blank?
         redirect_to action: "new"
       else
         super
+        flash[:notice] = nil
       end
     else
       super
+      flash[:notice] = nil
     end
   end
 
@@ -26,7 +32,21 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def login_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def blank_error_message(login_params)
+    error_messages = []
+    login_params.each do |key, value|
+      if value.blank?
+        error_messages << [key, '入力してください']
+      end
+    end
+    return error_messages.to_h
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
